7c4b34c7ab93b81fd2ff7728e28665d273b47bb9
Bryce512 <bryce.whitworth@gmail.com>
2024-11-21 17:13:16 -0700

    change agrees to agree in getcertif

--- .platform/hooks/postdeploy/00_getcertif.sh
+++ .platform/hooks/postdeploy/00_getcertif.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-sudo certbot -n -d splitit.is404.net  --nginx --agrees-tos --email rls21@byu.edu
+sudo certbot -n -d splitit.is404.net  --nginx --agree-tos --email rls21@byu.edu
