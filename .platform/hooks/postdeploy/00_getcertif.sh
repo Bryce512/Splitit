cb69cc439315cea769d4b8cb407c4f78ba8a3831
Bryce512 <bryce.whitworth@gmail.com>
2024-12-09 00:18:25 -0700

    update https

--- .platform/hooks/postdeploy/00_getcertif.sh
+++ .platform/hooks/postdeploy/00_getcertif.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-sudo certbot -n -d splitit.is404.net  --nginx --agree-tos --email rls21@byu.edu
+sudo certbot -n -d splitit.brycewhitworth.com  --nginx --agree-tos --email rls21@byu.edu
